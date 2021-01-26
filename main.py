85b4de53a40d7c6cbca35ffadc0b205eb2125148
alyssaurbanski23 <831554@mystma.org>
2021-01-26 17:08:43 +0000

    lastletter try 1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def lastLetter(minnesota):
+     
+     for i in range(0, len(w)):
+        if w[i] == "a":
+            return True
+
+        return False
+
+print (lastLetter(minnesota))
